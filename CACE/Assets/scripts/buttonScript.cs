9d1d78de385c2fdf6e79ca0763eff8741537db6d
Cade Anderson <cadeanderson99@gmail.com>
2019-01-17 12:53:13 -0800

    Door prototype

---
+++ CACE/Assets/scripts/buttonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+
+    public bool pressed = false;
+    Material myMaterial;
+
+    // Use this for initialization
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pressed = true;
+            myMaterial.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            pressed = false;
+            myMaterial.color = Color.blue;
+        }
+    }
+}
---
+++ CACE/Assets/scripts/doorScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour {
+
+    public bool prevButtonState;
+    public GameObject myButton;
+    buttonScript myButtonScript;
+    private IEnumerator coroutine;
+
+    float t = 0;
+    float interpos;
+
+    void Start ()
+    {
+        myButtonScript = myButton.GetComponent<buttonScript>();
+        prevButtonState = myButtonScript.pressed;
+    }
+	
+	void Update ()
+    {
+        if (myButtonScript.pressed != prevButtonState)
+        {
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+            
+            coroutine = Open(myButtonScript.pressed ? 1 : 0);
+            
+            StartCoroutine(coroutine);
+            prevButtonState = myButtonScript.pressed;
+            /*rb.detectCollisions = false;
+            renderer.enabled = false;*/
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("O");
+            coroutine = Open(1);
+            
+            StartCoroutine(coroutine);
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("P");
+            coroutine = Open(0);
+
+            StartCoroutine(coroutine);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StopCoroutine(coroutine);
+        }
+
+    }
+
+    IEnumerator Open(float goal)
+    {
+        Debug.Log("Coroutine");
+        while (goal >= t)
+        {      
+            t += 0.01f;
+
+            interpos = (0.59f * (1 - t) + 4 * t);
+            transform.position = new Vector3(transform.position.x, interpos, transform.position.z);
+            yield return null;
+            
+        }
+        while (goal <= t)
+        {
+            t -= 0.01f;
+
+            interpos = (0.59f * (1 - t) + 4 * t);
+            transform.position = new Vector3(transform.position.x, interpos, transform.position.z);
+            yield return null;
+
+        }
+    }
+}
