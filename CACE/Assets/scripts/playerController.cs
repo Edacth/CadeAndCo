1ee0277269eed3e4063f8289f0999b56ca24f4a4
Alexi Most <alexibmost@gmail.com>
2019-01-17 12:39:36 -0800

    Finished PlayerController

--- CACE/Assets/scripts/playerController.cs
+++ CACE/Assets/scripts/playerController.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public int speed = 10;
-
+    public int moveSpeed = 10;
+    public int rotationSpeed = 3;
     public Rigidbody rb;
+    Vector3 prevMousePos;
+
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-
-        // player movement
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        movement.Normalize();
-        rb.AddForce(movement * speed);
-
-
+        prevMousePos = Input.mousePosition;
     }
 
-  
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // player rotation
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 rotation = new Vector3(0, rotationSpeed, 0);
+        float dif = mousePos.x - prevMousePos.x;
+        if (dif < 0)
+        {
+            rb.transform.Rotate(rotation * -1);
+        }
+        if (dif > 0)
+        {
+            rb.transform.Rotate(rotation);
+        }
+        prevMousePos = mousePos;
+        // move forward back towards the center of screen
+
+        // player movement (WASD)
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));      
+        float rotationFromOrig = Vector3.Angle(new Vector3(0, 0, 1), rb.transform.forward);
+        if (rb.transform.forward.x < 0)
+        {
+            rotationFromOrig = 180 - rotationFromOrig + 180;
+        }
+
+        Quaternion rotationFromOriginQ = Quaternion.Euler(0, rotationFromOrig, 0);
+        Vector3 netMovement = rotationFromOriginQ * movement;
+
+        netMovement.Normalize();
+        
+        rb.MovePosition(rb.position + netMovement * (moveSpeed / 100f));
+
+        Debug.Log(rb.transform.forward);
+    }
+
 }
