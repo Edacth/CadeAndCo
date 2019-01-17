c6b3a164728634db3f9d2f8535f5d39a9b5fa54b
Alexi Most <alexibmost@gmail.com>
2019-01-17 14:28:14 -0800

    basic camera controller

---
+++ CACE/Assets/scripts/cameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public Camera thisCam;
+    int rotationSpeed = 1;
+    float prevMousePos;
+    float rot = 0;
+    // y angle of fov / 2
+    public int fovY = 70;
+
+    // Use this for initialization
+    void Start()
+    {
+        thisCam = GetComponent<Camera>();
+        prevMousePos = Input.mousePosition.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        float mousePos = Input.mousePosition.y;
+        Vector3 net = new Vector3(rotationSpeed, 0, 0);
+        if(prevMousePos - mousePos > 0 && rot < fovY)
+        {
+            thisCam.transform.Rotate(-net);
+            rot += rotationSpeed;
+        }
+        if (prevMousePos - mousePos < 0 && rot > -fovY)
+        {
+            thisCam.transform.Rotate(net);
+            rot -= rotationSpeed;
+        }
+        prevMousePos = mousePos;
+
+        Debug.Log(rot);
+	}
+}
