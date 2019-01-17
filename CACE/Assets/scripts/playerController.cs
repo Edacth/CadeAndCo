11267543c40b259339a7150903ff61ddee308988
Alexi Most <alexibmost@gmail.com>
2019-01-17 09:34:59 -0800

    PlayerController

--- CACE/Assets/scripts/playerController.cs
+++ CACE/Assets/scripts/playerController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 10;
+
+    public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+
+        // player movement
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement.Normalize();
+        rb.AddForce(movement * speed);
+
+
+    }
+
+  
 }
