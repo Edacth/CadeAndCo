b1d20d66ba45f39a41d4b3d9c3d46afa3c8edd80
Cade Anderson <cadeanderson99@gmail.com>
2019-01-23 12:31:08 -0800

    Pushing the player controller for Alexi

--- CACE/Assets/scripts/playerController.cs
+++ CACE/Assets/scripts/playerController.cs
@@ -1,34 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class playerController : MonoBehaviour {
+    public delegate void delRemoteControl();
 
     public float moveSpeed;
     public float rotationSpeed;
     public bool isHoldingKey = false;
 
+    //public List<delRemoteControl> RemoteControls = new List<delRemoteControl>();
+    public delRemoteControl RemoteControls;
+    
     private Vector2 mouseMovement;
-    private Transform rotationProbe;
     private static CharacterController controller;
     private Transform cam;
     private float rotationLock = 280f;
 
     private int interactionLength = 10; // how close you have to be to push buttons
-    Ray ray;
-    RaycastHit hit;
+    private Ray ray;
+    private RaycastHit rayHit;
 
-
-    // Use this for initialization
     void Start ()
     {
         controller = GetComponent<CharacterController>();
         cam = transform.GetChild(0);
         Cursor.visible = false;
-        ray = new Ray(controller.center, controller.transform.forward);
+        ray = new Ray(cam.position, cam.transform.forward);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         // This changes the player rotation
@@ -39,31 +40,6 @@ public class playerController : MonoBehaviour {
 
         // Up/Down
         deltaRotation = new Vector3(mouseMovement.y, 0, 0) * rotationSpeed;
-
-        rotationProbe.Rotate(deltaRotation);
-        //float deltaRot = cam.transform.localRotation.x + mouseMovement.y * rotationSpeed;
-        
-        if (rotationProbe.localRotation.y == 180) 
-        {
-            /*float diff = rotationLock - cam.transform.localRotation.eulerAngles.x;
-            if (cam.transform.eulerAngles.x < rotationLock && cam.transform.eulerAngles.x  < 90)
-            {
-                
-                Debug.Log("true");
-            }*/
-            
-
-            /*if (Input.GetKeyDown(KeyCode.E))
-            {
-                deltaRotation = new Vector3(diff, 0, 0);
-            }*/
-
-            //rotationProbe.eulerAngles
-        }
-
-        print("LocalRotation: " + cam.transform.eulerAngles);
-
-
         cam.transform.Rotate(deltaRotation);
               
         // This handles player movement
@@ -71,16 +47,11 @@ public class playerController : MonoBehaviour {
         controller.Move(gameObject.transform.forward * movement.z * moveSpeed);
         controller.Move(gameObject.transform.right * movement.x * moveSpeed );
 
-        // update ray
-        // what is the better way to do this?
-        ray = new Ray(cam.position, cam.transform.forward);
-        // Debug.DrawRay(controller.center, controller.transform.forward, Color.red, 100);
-
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            CheckForInteractable();
-        }
-       
+        // Raycasting
+        ray = new Ray(cam.position, cam.transform.forward * interactionLength);
+        Debug.DrawRay(cam.position, cam.transform.forward * interactionLength, Color.red, 0.01f);
+        CheckForInteractable();
+      
     }
 
     void OnTriggerEnter(Collider other)
@@ -94,15 +65,19 @@ public class playerController : MonoBehaviour {
 
     void CheckForInteractable()
     {
-    
-        if (Physics.Raycast(ray, out hit))
+        LayerMask mask = LayerMask.GetMask("interactable");
+        if (Physics.Raycast(ray, out rayHit, interactionLength, mask))
         {
+            
             bool didHit = false;
-            if (hit.collider.gameObject.tag.Equals("button2"))
+            if (rayHit.collider.gameObject.tag.Equals("button"))
             {
+                //rayHit.collider.GetComponent<Renderer>().
                 didHit = true;
+                //Debug.Log("I hit" + rayHit.transform.name);
+                RemoteControls.Invoke();
             }
-            hit.collider.gameObject.GetComponent<button2Script>().PlayerLooking(didHit);
+            //rayHit.collider.gameObject.GetComponent<button2Script>().PlayerLooking(didHit);
         }
   
     }
