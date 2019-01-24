f076944d7ba93fccf6d99972fd16533f9a237268
Gavin Coffelt <gavincoffelt@yahoo.com>
2019-01-24 14:30:55 -0800

    yay GAvin

---
+++ CACE/Assets/scripts/levelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelManager : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("mazeScene");
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("I want to Quit!");
+        Application.Quit();
+    }
+    public void LoadLose()
+    {
+        SceneManager.LoadScene("loseScene");
+    }
+
+}
+
---
+++ CACE/Assets/scripts/spikes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+public class spikes : MonoBehaviour {
+//GameObject DeathTest;
+    float velocity = 1;
+    
+    Vector3 bobbing = new Vector3();
+    Vector3 ogPos = new Vector3();
+    Vector3 temp = new Vector3 ();
+
+    void Start () {
+        //DeathTest = GameObject.Find("DeathTest");
+        ogPos = this.transform.position;
+
+    }
+
+    void Update() {
+
+
+
+        bobbing.y += velocity * Time.deltaTime;
+        temp = new Vector3(ogPos.x, bobbing.y, ogPos.z);
+        if (this.transform.position.y >= ogPos.y + 1) { velocity = -5; }
+        else if (this.transform.position.y <= ogPos.y-2) { velocity = 1; }
+                this.transform.position = temp;
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene("loseScene");
+    }
+    
+}
